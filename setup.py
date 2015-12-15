503d35ced2e36354c34f9cc5d739fe2e9c1a78b3
Ferry Boender <ferry.boender@gmail.com>
2015-12-15 22:29:42 +0100

    Python setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='jsonxs',
+      version='0.1',
+      description='Use path expression strings to get and set values in JSON',
+      long_description=open('README.rst').read(),
+      url='https://github.com/fboender/jsonxs',
+      author='Ferry Boender',
+      author_email='ferry.boender@gmail.com',
+      license='MIT',
+      packages=['jsonxs'],
+      zip_safe=True)
