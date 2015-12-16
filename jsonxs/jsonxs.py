e5de2fa5becfc67f76fd35232b5a8bb0808a454b
Ferry Boender <ferry.boender@gmail.com>
2015-12-16 09:27:52 +0100

    Small refactoring for code clarity.

--- jsonxs/jsonxs.py
+++ jsonxs/jsonxs.py
@@ -89,29 +89,32 @@ def tokenize(expr):
     cur_token = ''
 
     for c in expr:
-        if c == '\\':
-            # Next char will be escaped
-            escape = True
-            continue
-        elif not escape and c == '[':
-            # Next token is of type index (list)
-            if len(cur_token) > 0:
-                tokens.append(cur_token)
-                cur_token = ''
-        elif not escape and c == ']':
-            # End of index token. Next token defaults to a key (dict)
-            if len(cur_token) > 0:
-                tokens.append(int(cur_token))
-                cur_token = ''
-        elif not escape and c == '.':
-            # End of key token. Next token defaults to a key (dict)
-            if len(cur_token) > 0:
-                tokens.append(cur_token)
-                cur_token = ''
-        else:
-            # Append char to token name
+        if escape == True:
             cur_token += c
-        escape = False
+            escape = False
+        else:
+            if c == '\\':
+                # Next char will be escaped
+                escape = True
+                continue
+            elif c == '[':
+                # Next token is of type index (list)
+                if len(cur_token) > 0:
+                    tokens.append(cur_token)
+                    cur_token = ''
+            elif c == ']':
+                # End of index token. Next token defaults to a key (dict)
+                if len(cur_token) > 0:
+                    tokens.append(int(cur_token))
+                    cur_token = ''
+            elif c == '.':
+                # End of key token. Next token defaults to a key (dict)
+                if len(cur_token) > 0:
+                    tokens.append(cur_token)
+                    cur_token = ''
+            else:
+                # Append char to token name
+                cur_token += c
     if len(cur_token) > 0:
         tokens.append(cur_token)
 
